d473719119cc481e47260147e39b654e1474f68a
Jakob McBroome <jmcbroome@gmail.com>
2022-11-29 11:08:44 -0800

    numerous fixing to handle weird annotation column counts

--- loop.sh
+++ loop.sh
@@ -1,8 +1,9 @@
 set -e 
 #reset.
+echo "Resetting."
 mv *all.masked* temp
 mv *metadata.tsv.gz temp
-rm public*
+rm public* || true
 mv temp/* .
 rm floating_paths.txt
 rm floating_samples_clades.txt
@@ -11,8 +12,10 @@ touch floating_paths.txt
 touch floating_samples_clades.txt
 for f in *pb.gz ; do 
     export PREFIX=${f%.all.masked.pb.gz} ; 
-    python3 strip_annotations.py $PREFIX.all.masked.pb.gz $PREFIX.cleaned.pb ;
-    matUtils annotate -T 4 -i $PREFIX.cleaned.pb -P floating_paths.txt -c floating_samples_clades.txt -o $PREFIX.pb ; 
+    echo "Analyzing "$PREFIX ;
+    python3 strip_annotations.py $PREFIX.all.masked.pb.gz $PREFIX.cleaned.pb > $PREFIX.designated.txt ;
+    matUtils annotate -T 4 -i $PREFIX.cleaned.pb -P floating_paths.txt -c floating_samples_clades.txt -o $PREFIX.tmp.pb ; 
+    python3 strip_annotations.py $PREFIX.tmp.pb $PREFIX.pb > $PREFIX.second_designated.txt ;
     gzip $PREFIX.pb ; 
     cd automate-lineages-prototype ;
     snakemake -c1 -s flag_lineages.smk ../$PREFIX.proposed.report.tsv ; 
